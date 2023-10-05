b879250b8a64a6ee0007bfd43961b591e945f705
Tricia Swift <tricia.swift8@gmail.com>
2023-10-05 10:01:02 -0500

    wip

--- swimming/frog.py
+++ swimming/frog.py
@@ -10,6 +10,9 @@ class Frog:
         self.swimming = True
         self.food = food
 
+    def __str__(self):
+        return f"{self.name} is a {self.species}"
+
     def feed(self):
         print(
             f'{self.name} was fed {self.food} on {self.date_added.strftime("%m/%d/%Y")}')
