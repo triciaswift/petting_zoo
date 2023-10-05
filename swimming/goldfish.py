0ca937e74120160f3fedfbb416d55cf3f0470ba8
Tricia Swift <tricia.swift8@gmail.com>
2023-10-05 09:49:54 -0500

    wip

--- swimming/goldfish.py
+++ swimming/goldfish.py
@@ -3,8 +3,13 @@ from datetime import date
 
 class Goldfish:
 
-    def __init__(self, name, species):
+    def __init__(self, name, species, food):
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.swimming = True
+        self.food = food
+
+    def feed(self):
+        print(
+            f'{self.name} was fed {self.food} on {self.date_added.strftime("%m/%d/%Y")}')
